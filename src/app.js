524f3cf91f4308a669c915969d55b5df84c7cb2e
Patrick Teglia <pteglia@gmail.com>
2015-09-11 23:09:47 -0700

    Pretty much got the basics up and running.

--- src/app.js
+++ src/app.js
@@ -7,54 +7,63 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
+var formatShortDate = function(d) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+  var dd = d.getDate();
+  var mm = monthNames[d.getMonth()];
+  var yy = d.getFullYear();
+
+  return mm + ' ' + dd + ', '+yy;
+};
+
+var deadpoolDate = new Date(2016, 1, 12);
+var todaysDate = new Date();
+var isItOutYetSub = '';
+var isItOutYetBody = '';
+
+var options = {
+  weekday: "long", year: "numeric", month: "short",
+  day: "numeric", hour: "2-digit", minute: "2-digit"
+};
+
+if (todaysDate >= deadpoolDate) {
+  isItOutYetSub = 'YES YES YES!';
+  isItOutYetBody = 'Time to touch yourse... go see a movie!';
+}
+else {
+  isItOutYetSub = 'No.';
+  isItOutYetBody = "Counting seconds? Click a button!";
+}
+
+
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  title: 'Is Deadpool Out Yet?',
+  subtitle: isItOutYetSub,
+  body: isItOutYetBody
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
 main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
   var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
+  var secondsLeft = Math.round((deadpoolDate - todaysDate) / 1000);
+
+  card.title("There's Only");
+  card.subtitle(secondsLeft);
+  card.body("Seconds (that's " + Math.round(secondsLeft / 86400) + " days) until it is out. Don't worry, you will make it!");
   card.show();
+  todaysDate = new Date();
+  var interval = setInterval(function() {
+
+    if (todaysDate <= deadpoolDate) {
+      todaysDate = new Date();
+      var timeleft = Math.round((deadpoolDate - todaysDate) / 1000 ); 
+      card.subtitle(timeleft);
+    }
+  }, 1000);
 });
